1975109c7495f3db77c5f126df4fbe0f81fea2ea
bhumijgupta <bhumijgupta@gmail.com>
2020-11-28 22:18:54 +0530

    NEW: Add basic alpha example for classproxima proctoring
    Signed-off-by: bhumijgupta <bhumijgupta@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Proctor from "@classproxima/proctoring";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    let proctoring = async () => {
+      let status = await proctor.startMonitoring();
+      console.log(status);
+    };
+    let proctor = new Proctor("api-key", "student-id");
+    proctor.on("mouse-enter", () => {
+      setEvents((events) => [...events, "Mouse entered the window"]);
+      console.log("mouse-enter");
+    });
+    proctor.on("mouse-leave", () => {
+      setEvents((events) => [...events, "Mouse exited from window"]);
+      console.log("mouse-leave");
+    });
+    proctor.on("page-back-to-foreground", () => {
+      setEvents((events) => [
+        ...events,
+        "Tab in focus/ Window brought to foreground",
+      ]);
+      console.log("page-back-to-foreground");
+    });
+    proctor.on("page-to-background", () => {
+      setEvents((events) => [
+        ...events,
+        "Tab switched/ Window sent to background",
+      ]);
+      console.log("page-to-background");
+    });
+    proctoring();
+    return () => {
+      proctor.stopMonitoring();
+    };
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Logs</h1>
+        <h6>
+          Try navigating away from screen, switching tabs to generate logs
+        </h6>
+        <div>
+          {events.map((event) => {
+            return <li style={{ fontSize: "0.75em" }}>{event}</li>;
+          })}
+        </div>
       </header>
     </div>
   );
